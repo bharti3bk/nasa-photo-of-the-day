da5fb92abac68de2b9a3f5c9f8c103873970305e
bharti3bk <bharti3bk@gmail.com>
2019-07-18 17:56:55 -0400

    Added cardComponent via semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import NasaParentComponent from './Components/NasaParentComponent';
+import NasaProjectTimeLine from './Components/NasaProjectTimeLine';
 
 function App() {
   return (
     <div className="App">
       <NasaParentComponent />
+      <NasaProjectTimeLine >
+       <NasaProjectTimeLine />
+      </NasaProjectTimeLine>
     </div>
   );
 }
--- src/Components/NasaProjectTimeLine.js
+++ src/Components/NasaProjectTimeLine.js
@@ -1,1 +1,24 @@
 import React from 'react';
+import '../App.css';
+
+function NasaProjectTimeLine (props){
+  return ( 
+      <div id = "uicard">
+          <div className = "content">
+          <div id="header">Project Timeline</div>
+            <hr id="HR"/>
+              <div className = "extra content"> 
+              <h4 className="ui sub header">Activity</h4>
+              </div>
+              <div className="ui small feed">
+              <div className="summary">
+            <p>Elliot Fu added Jenny Hess to the project</p>
+            <p>Stevie Feliciano was added as an Administrator</p>
+            <button id="uibutton">Join Project</button>
+          </div>
+              </div>
+         </div>
+      </div>
+  );
+}  
+export default NasaProjectTimeLine;
